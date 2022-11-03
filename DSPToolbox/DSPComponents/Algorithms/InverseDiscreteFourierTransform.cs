a5fa2008291a6e53c3dc04ce40c9d5a7989bd277
yusufnasserdev <yusufnassereng@gmail.com>
2022-11-03 20:07:40 +0200

    Implemented InverseDiscreteFourierTransform class
    Co-authored-by: Yousef Mostafa <71218747+YOUsefmostaf@users.noreply.github.com>

--- DSPToolbox/DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
+++ DSPToolbox/DSPComponents/Algorithms/InverseDiscreteFourierTransform.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DSPAlgorithms.DataStructures;
+using System.Numerics;
 
 namespace DSPAlgorithms.Algorithms
 {
@@ -14,7 +15,42 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<Complex> values = new List<Complex>();
+            List<Complex> complexNums = new List<Complex>();
+
+            for (int i = 0; i < InputFreqDomainSignal.FrequenciesAmplitudes.Count; i++)
+            {
+                complexNums.Add(Complex.FromPolarCoordinates(InputFreqDomainSignal.FrequenciesAmplitudes[i],
+                    InputFreqDomainSignal.FrequenciesPhaseShifts[i]));
+            }
+
+            double termAnswer = 0;
+
+            for (int i = 0; i < InputFreqDomainSignal.FrequenciesAmplitudes.Count; i++)
+            {
+                values.Add(termAnswer);
+
+                for (int j = 0; j < InputFreqDomainSignal.FrequenciesAmplitudes.Count; j++)
+                {
+                    double ePower = (2 * Math.PI * i * j) / InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+                    values[i] +=
+                        (complexNums[j].Real * Math.Cos(ePower) + Complex.ImaginaryOne
+                        * complexNums[j].Real * Math.Sin(ePower))
+                        + (complexNums[j].Imaginary * -1 * Math.Cos(ePower) +
+                        complexNums[j].Imaginary * -1 * Math.Sin(ePower));
+                }
+
+                values[i] /= InputFreqDomainSignal.FrequenciesAmplitudes.Count;
+            }
+
+            List<float> realPart = new List<float>();
+
+            for (int i = 0; i < InputFreqDomainSignal.FrequenciesAmplitudes.Count; i++)
+            {
+                realPart.Add( (float) values[i].Real);
+            }
+
+            OutputTimeDomainSignal = new Signal(realPart, false);
         }
     }
 }
