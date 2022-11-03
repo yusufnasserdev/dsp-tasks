57bff0b3da7fd75b06ff7c0829acbdd6a2a86069
yusufnasserdev <yusufnassereng@gmail.com>
2022-11-03 19:52:47 +0200

    Implemented DiscreteFourierTransform class
    Co-authored-by: Yousef Mostafa <71218747+YOUsefmostaf@users.noreply.github.com>

--- DSPToolbox/DSPComponents/Algorithms/DiscreteFourierTransform.cs
+++ DSPToolbox/DSPComponents/Algorithms/DiscreteFourierTransform.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,33 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
-            throw new NotImplementedException();
+            List<float> amplitudes = new List<float>();
+            List<float> phaseShifts = new List<float>();
+
+            float ePower, realPart, imaginaryPart;
+
+            for (int i = 0; i < InputTimeDomainSignal.Samples.Count; i++)
+            {
+                realPart = 0;
+                imaginaryPart = 0;
+
+                for (int j = 0; j < InputTimeDomainSignal.Samples.Count; j++)
+                {
+                    ePower = (2 * (float) Math.PI * i * j) / InputTimeDomainSignal.Samples.Count;
+                    realPart += InputTimeDomainSignal.Samples[j] * (float) Math.Cos(ePower);
+                    imaginaryPart += -InputTimeDomainSignal.Samples[j] * (float) Math.Sin(ePower);
+                }
+
+                double amplitude = Math.Sqrt(Math.Pow(realPart, 2) + Math.Pow(imaginaryPart, 2));
+                amplitudes.Add( (float) amplitude);
+                double phaseShift = Math.Atan2(imaginaryPart, realPart);
+                phaseShifts.Add((float) phaseShift);
+            }
+
+            OutputFreqDomainSignal = new Signal(InputTimeDomainSignal.Samples, false);
+            OutputFreqDomainSignal.FrequenciesAmplitudes = amplitudes;
+            OutputFreqDomainSignal.FrequenciesPhaseShifts = phaseShifts;
+
         }
     }
 }
