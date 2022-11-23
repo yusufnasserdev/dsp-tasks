25f5dc206a6ffb3135b095945d76a0500cfe90d4
yusufnasserdev <yusufnassereng@gmail.com>
2022-11-23 20:55:49 +0200

    Finished TimeDelay algorithm
    Co-authored-by: Yousef Mostafa <71218747+YOUsefmostaf@users.noreply.github.com>

--- DSPToolbox/DSPComponents/Algorithms/TimeDelay.cs
+++ DSPToolbox/DSPComponents/Algorithms/TimeDelay.cs
@@ -16,6 +16,22 @@ namespace DSPAlgorithms.Algorithms
 
         public override void Run()
         {
+            DirectCorrelation dc = new DirectCorrelation();
+            dc.InputSignal1 = InputSignal1;
+            dc.InputSignal2 = InputSignal2;
+
+            dc.Run();
+
+            float maxAbslouteVal = dc.OutputNonNormalizedCorrelation.Max();
+            int lagj = 0;
+            for (int i = 0; i < dc.OutputNonNormalizedCorrelation.Count; i++)
+                if (dc.OutputNonNormalizedCorrelation[i] == maxAbslouteVal)
+                {
+                    lagj = i;
+                    break;
+                }
+
+            OutputTimeDelay = lagj * InputSamplingPeriod;
 
         }
     }
